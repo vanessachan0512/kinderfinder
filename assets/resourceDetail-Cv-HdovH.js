72e5b2d03369e6eb186476401c5ac185291bfeff
vanessachan0512 <22241027@life.hkbu.edu.hk>
2026-04-19 14:33:29 +0800

    Updates

--- assets/resourceDetail-Cv-HdovH.js
+++
@@ -1,1 +0,0 @@
-import{z as r,r as s}from"./index-DxQ2uLV5.js";const c=r("resourceDetail",()=>{const e=s(null),a=s(null);return{passedArticle:e,passedIsBookmarked:a,setPassedData:(t,l)=>{e.value=t,a.value=l},clearPassedData:()=>{e.value=null,a.value=null}}});export{c as u};
